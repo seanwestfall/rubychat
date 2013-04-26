0bb84eb8936bdcddb73519fd0baac7fd370d3597
alphonse23 <alphonse23@gmail.com>
2013-04-26 11:50:55 -0700

    stuff

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'jquery-rails'
 gem 'thin'
 gem 'em-websocket'
 gem 'em_aws'
+gem 'capistrano'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
---
+++ config/deploy/production.rb
@@ -0,0 +1,5 @@
+set :user, "webuser"
+server "ec2-54-241-235-11.us-west-1.compute.amazonaws.com"
+:primary => true
+ssh_options[:key] = ["#{ENV}['HOME']}/.ssh/id_dsa.pub"]
+
