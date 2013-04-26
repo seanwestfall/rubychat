532ec39e4153bc5d56251c55b02bf0077bc1da54
alphonse23 <alphonse23@gmail.com>
2013-04-26 12:33:47 -0700

    next attempt 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,9 @@ set :application, "rubychat"
 set :repository,  "git@github.com:alphonse23/rubychat.git"
 set :scm, :git
 set :user, "webuser"
+set :use_sudo, false
 
-# set :deploy_to, ""
+set :deploy_to, "/home/webuser/rubychat"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
